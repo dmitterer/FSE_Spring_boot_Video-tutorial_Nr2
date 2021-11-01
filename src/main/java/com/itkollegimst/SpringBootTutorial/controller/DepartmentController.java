637630dca7651598bdb22a3742f4d10b07838b4d
Dominik Mitterer <dmitterer@tsn.at>
2021-11-01 17:42:55 +0100

    Klasse "DepartmentController", " DepartmentServiceImpl"," DepartmentService" und "DepartmentImpl"erweitern.
    Doku siehe Word Unterpunkt 5.9.3

--- src/main/java/com/itkollegimst/SpringBootTutorial/controller/DepartmentController.java
+++ src/main/java/com/itkollegimst/SpringBootTutorial/controller/DepartmentController.java
@@ -4,9 +4,12 @@ import com.itkollegimst.SpringBootTutorial.entity.Department;
 import com.itkollegimst.SpringBootTutorial.service.DepartmentService;
 import com.itkollegimst.SpringBootTutorial.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -21,5 +24,11 @@ public class DepartmentController {
 
     }
 
+    @GetMapping( "/departments")
+    public List<Department> fetchDepartmentList(){
+
+        return departmentService.fetchDepartmentList();
+
+    }
 
 }
--- src/main/java/com/itkollegimst/SpringBootTutorial/service/DepartmentService.java
+++ src/main/java/com/itkollegimst/SpringBootTutorial/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.itkollegimst.SpringBootTutorial.service;
 
 import com.itkollegimst.SpringBootTutorial.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+   public List<Department> fetchDepartmentList();
 }
