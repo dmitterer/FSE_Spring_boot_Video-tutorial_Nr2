deeeba7d6d13aace60a9e4f456058373d41b29ff
Dominik Mitterer <dmitterer@tsn.at>
2021-11-04 19:16:40 +0100

    Ergänzung von Logger in der Klasse "DepartmentController".
    Siehe Word-doku Unterpunkt 5.11.1

--- src/main/java/com/itkollegimst/SpringBootTutorial/controller/DepartmentController.java
+++ src/main/java/com/itkollegimst/SpringBootTutorial/controller/DepartmentController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.annotation.JsonValueInstantiator;
 import com.itkollegimst.SpringBootTutorial.entity.Department;
 import com.itkollegimst.SpringBootTutorial.service.DepartmentService;
 import com.itkollegimst.SpringBootTutorial.service.DepartmentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +19,12 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid() @RequestBody Department department) {
+
+        LOGGER.info("Inside saveDepartment of DepartmentController");
 
         return departmentService.saveDepartment(department);
 
@@ -27,6 +33,7 @@ public class DepartmentController {
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
 
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
 
     }
